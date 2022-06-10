8491a77525a1e1a6cf069f81eb742a1dbbbd50a1
Bearcode Teeeps <bearcodeteeeps@gmail.com>
2022-06-10 11:19:19 -0500

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
+}
